be5d032b9f774a79c49ad7c31e3a3fe81200d496
wilson496 <Cameron.Wilson@nttdata.com>
2023-02-12 16:52:01 -0800

    [FIX] - fix jumble word bug, check for successful jumble, return proper audit len

--- service/api/jumble_audit.py
+++ service/api/jumble_audit.py
@@ -1,5 +1,5 @@
 # from typing import Union
-from random import shuffle
+from random import sample
 
 from fastapi import APIRouter
 # from pydantic import BaseModel
@@ -7,15 +7,27 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+api_calls = []
+
 
 # TODO: Enforce the use of a single word?
 @router.post("/jumble/{word}")
 def post_word_to_jumble(word: str):
-    word = list(word)
-    shuffle(word)
-    return ''.join(word)
+
+    # TODO: Enforce single word (maybe?)
+
+    # Make sure shuffled word DOES NOT match original
+    # Initial testing with word 'foo' would return
+    # a "shuffled" result of foo sometimes
+    while (True):
+        shuffled = ''.join(sample(word, len(word)))
+        if shuffled != word:
+            break
+    api_calls.append({"word": str(word), "shuffled_word": str(shuffled)})
+    return shuffled
 
 
 @router.get("/audit")
 def get_last_ten_jumbles():
-    pass
+    list = api_calls[-10:]
+    return list
